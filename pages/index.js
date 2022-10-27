6d41248c5482ddeb916c58d06c1db0e17a21b7a7
Feline Huhn <feline.huhn@gmx.de>
2022-10-27 17:23:53 +0200

    change title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export default function Home() {
   return (
     <div>
-      <Title>Hallo</Title>
+      <Title>Delicious Recipes</Title>
       <Description>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
